cd334b91851d9776da7af2d4efab750e8df4df54
Kirill Kozinets <44705397+KirillKozinets@users.noreply.github.com>
2020-11-22 11:53:44 +0700

    Deleted unused code

--- app/src/main/java/com/sgc/speedometer/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/sgc/speedometer/ui/base/BaseViewModel.kt
@@ -1,20 +1,15 @@
 package com.sgc.speedometer.ui.base
 
-import androidx.databinding.ObservableBoolean
 import androidx.lifecycle.ViewModel
 import com.sgc.speedometer.data.DataManager
 import io.reactivex.disposables.CompositeDisposable
 
 abstract class BaseViewModel(val dataManager: DataManager) : ViewModel() {
-    val isLoading = ObservableBoolean()
     private val mCompositeDisposable: CompositeDisposable = CompositeDisposable()
 
     override fun onCleared() {
         mCompositeDisposable.dispose()
         super.onCleared()
     }
-
-    val compositeDisposable: CompositeDisposable
-        get() = mCompositeDisposable
 
 }
