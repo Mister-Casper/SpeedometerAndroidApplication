1c8f8df78ee9b80599cb91f435e81b1203e7a10b
Kirill Kozinets <44705397+KirillKozinets@users.noreply.github.com>
2020-11-26 15:05:34 +0700

    Connected firebase

--- app/src/main/java/com/sgc/speedometer/ui/speedometer/SpeedometerActivity.kt
+++ app/src/main/java/com/sgc/speedometer/ui/speedometer/SpeedometerActivity.kt
@@ -47,7 +47,6 @@ class SpeedometerActivity : BaseActivity<ActivitySpeedometerBinding, Speedometer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         if (savedInstanceState == null) {
             ActivityCompat.requestPermissions(
                 this, arrayOf(
