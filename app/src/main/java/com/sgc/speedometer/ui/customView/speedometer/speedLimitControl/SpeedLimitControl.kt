65e30d3422d36292a895ae786804703cc944f50e
Kirill Kozinets <44705397+KirillKozinets@users.noreply.github.com>
2020-12-06 14:00:53 +0700

    Refactoring

--- app/src/main/java/com/sgc/speedometer/ui/customView/speedometer/speedLimitControl/SpeedLimitControl.kt
+++ app/src/main/java/com/sgc/speedometer/ui/customView/speedometer/speedLimitControl/SpeedLimitControl.kt
@@ -3,19 +3,17 @@ package com.sgc.speedometer.ui.customView.speedometer.speedLimitControl
 class SpeedLimitControl(var speedLimitControlObserver: SpeedLimitControlObserver, var speedLimit: Int) {
 
     private var isSpeedLimitExceeded = false
-    private var isNotified = false
 
     fun checkSpeedLimit(currentSpeed: Int) {
         if (currentSpeed > speedLimit) {
-            if(!isNotified) {
+            if (!isSpeedLimitExceeded) {
                 speedLimitControlObserver.speedLimitExceeded()
                 isSpeedLimitExceeded = true
-                isNotified = true
             }
         } else {
             if (isSpeedLimitExceeded) {
                 speedLimitControlObserver.speedLimitReturned()
-                isNotified = false
+                isSpeedLimitExceeded = false
             }
         }
     }
